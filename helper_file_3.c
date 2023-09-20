e79715b8f61148c1898be809629cdace2df4e633
EMarvelM <marvelousegbe19@gmail.com>
2023-09-20 23:02:54 +0100

    counts string length

--- helper_file_3.c
+++ helper_file_3.c
@@ -23,3 +23,30 @@ void newline_remover(char *str)
 		str[i] = '\0';
 	}
 }
+
+
+/**
+ *_stringlen - counts string length
+ *@sstring: string to be counted
+ *
+ * Return: length of the string
+ */
+
+int _stringlen(char *sstring)
+{
+	if ((shell_isatty(STDIN_FILENO)) == 1)
+	{
+		int len = 0;
+
+		if (sstring == NULL)
+			return (len);
+
+		while (sstring[len] != '\0')
+		{
+			len++;
+		}
+
+		return (len);
+	}
+	return (0);
+}
