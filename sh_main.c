23a25912136487aef82cd3a07afc3cc2c702136a
Ozioma EMEREM <code.emerem@gmail.com>
2023-09-20 23:41:31 +0100

    add function

---
+++ sh_main.c
@@ -0,0 +1,14 @@
+#include "simple_shell.h"
+
+/**
+ * Auth: Egbe Marvelous M
+ *               DC Ozioma
+ *
+ * Description:
+ * my main functions for main.c
+ */
+
+		char **my_cmds = NULL;
+		char *user_input = NULL;
+		char *program_name = NULL;
+		int status = 0;
