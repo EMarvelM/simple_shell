561b75ddba67a873513aa1e86e8c33da7f200fb5
Ozioma EMEREM <code.emerem@gmail.com>
2023-09-21 00:14:17 +0100

    complete simple shiell

--- simple_shell.h
+++ simple_shell.h
@@ -38,3 +38,48 @@ extern char **environ;
 /* Store the user input */
 extern char *user_input;
 extern char **my_cmds;
+
+/* Store the name of the program*/
+extern char *program_name;
+extern int status;
+
+int shell_isatty(int fd);
+
+/* help_0- printing, tokenizing & removing newline characters */
+void shell_printer(char *sstring, int output_stream);
+char **str_tokenizer(char *input_str, char *_delimita);
+void newline_remover(char *str);
+int _stringlen(char *sstring);
+void _strcopy(char *source, char *dest);
+
+/* help_2 */
+int _str_comp(char *first, char *second);
+char *_str_concat(char *destination, char *source);
+int _str_spn(char *str1, char *str2);
+int _strcspn(char *str1, char *str2);
+char *_strchr(char *s, char c);
+
+/* help_3 */
+char *_strtok_r(char *sstring, char *_delimita, char **save_ptr);
+int __atoi(char *s);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void ctrl_c_handler(int signum);
+void remove_comment(char *input);
+
+/* utilities */
+int parse_command(char *command);
+void execute_command(char **cmd_token, int command_type);
+char *check_path(char *command);
+void (*get_cmd_function(char *))(char **);
+char *_getenv(char *name);
+
+/* implementing the built_in function */
+void current_env(char **cmd_token __attribute__((unused)));
+void exiting_program(char **cmd_token);
+
+/* main */
+extern void uninteractive(void);
+extern void setup_init(char **current_cmd, int cmd_type);
+
+
+#endif /* SIMPLE_SHELL */
