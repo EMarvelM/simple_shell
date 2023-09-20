d3299ad5c967c2dc795863099357ea8639aa587b
Ozioma EMEREM <code.emerem@gmail.com>
2023-09-20 23:01:13 +0100

    added function 2

--- helper_file_3.c
+++ helper_file_3.c
@@ -23,3 +23,27 @@ void newline_remover(char *str)
 		str[i] = '\0';
 	}
 }
+
+/**
+ *_strcopy - copies a string to another buffer
+ *@source: source to copy from
+ *@dest: destination to copy to
+ *
+ * Return: void
+ */
+
+
+void _strcopy(char *source, char *dest)
+{
+	if ((shell_isatty(STDIN_FILENO)) == 1)
+	{	int i = 0;
+
+
+		while (source[i] != '\0')
+		{
+			dest[i] = source[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+}
