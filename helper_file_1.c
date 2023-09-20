4dfe87ba2aad4392c1cc6631172556dea4997de6
EMarvelM <marvelousegbe19@gmail.com>
2023-09-20 22:45:02 +0100

    prints a string to stdout (user defined)

--- helper_file_1.c
+++ helper_file_1.c
@@ -21,3 +21,41 @@ void shell_printer(char *sstring, int output_stream)
 		}
 	}
 }
+
+
+/**
+ *_str_concat - concatenates two strings
+ *@destination: the first string to be concatenated
+ *@source:  the second string to be concatenated
+ *
+ * Return: returns the address of the new string
+ */
+char *_str_concat(char *destination, char *source)
+{
+	if ((shell_isatty(STDIN_FILENO)) == 1)
+	{
+		char *new_string = NULL;
+		int len_dest = _stringlen(destination);
+		int len_source = _stringlen(source);
+		int i = 0;
+
+		new_string = malloc(sizeof(*new_string) * (len_dest + len_source + 1));
+
+		while (destination[i] != '\0')
+		{
+			new_string[i] = destination[i];
+			i++;
+		}
+
+		i = 0;
+		while (source[i] != '\0')
+		{
+			new_string[len_dest + i] = source[i];
+			i++;
+		}
+
+		new_string[len_dest + len_source] = '\0';
+		return (new_string);
+	}
+	return (NULL);
+}
