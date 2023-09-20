8e88281b7d7a41936b9e9024bced0dd09a20e48a
EMarvelM <marvelousegbe19@gmail.com>
2023-09-20 23:28:00 +0100

    reallocates a memory block (user deined)

---
+++ helper_file_5.c
@@ -0,0 +1,48 @@
+#include "simple_shell.h"
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: pointer to the memory previously allocated with a call to malloc
+ * @old_size: size of ptr
+ * @new_size: size of the new memory to be allocated
+ *
+ * Return: pointer to the address of the new memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *temp_block;
+	unsigned int i;
+
+	if (ptr == NULL)
+	{
+		temp_block = malloc(new_size);
+		return (temp_block);
+	}
+	else if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	else if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else
+	{
+		temp_block = malloc(new_size);
+		if (temp_block != NULL)
+		{
+			i = 0;
+			while (i < old_size && i < new_size)
+			{
+				*((char *)temp_block + i) = *((char *)ptr + i);
+				i++;
+			}
+			free(ptr);
+			return (temp_block);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
