07aa379be43a5794593f8e23822c82a0035855dc
EMarvelM <marvelousegbe19@gmail.com>
2023-09-20 23:47:38 +0100

    function to exit the shell

--- shell_builtins.c
+++ shell_builtins.c
@@ -20,3 +20,52 @@ void current_env(char **cmd_token __attribute__((unused)))
 		}
 	}
 }
+
+/**
+ * exiting_program - exits the shell
+ * @cmd_token: command entered
+ *
+ * Return: void
+ */
+void exiting_program(char **cmd_token)
+{
+	if ((shell_isatty(STDIN_FILENO)) == 1)
+	{
+		int num_token = 0;
+		int arg = 0;
+
+		for (; cmd_token[num_token] != NULL; num_token++)
+			;
+		if (num_token == 1)
+		{
+			free(cmd_token);
+			free(user_input);
+			free(my_cmds);
+			exit(status);
+		}
+		else if (num_token == 2)
+		{
+			arg = __atoi(cmd_token[1]);
+
+			if (arg == -1)
+			{
+				shell_printer(program_name, STDERR_FILENO);
+				shell_printer(": 1: exit: bad number: ", STDERR_FILENO);
+				shell_printer(cmd_token[1], STDERR_FILENO);
+				shell_printer("\n", STDERR_FILENO);
+				status = 2;
+			}
+			else
+			{
+				free(user_input);
+				free(cmd_token);
+				free(my_cmds);
+				exit(arg);
+			}
+		}
+		else
+		{
+		shell_printer("$: exit takes only 1 argument\n", STDERR_FILENO);
+		}
+	}
+}
